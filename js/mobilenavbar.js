9de1dedca5fb6107455c4a620e434ff02b9696c2
Shinto4 <cristianoferrara.2006@gmail.com>
2022-04-15 22:42:43 +0200

    update

---
+++ js/mobilenavbar.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.getElementById('menu')
+    var hamburgericon = document.getElementById('hamburgericon')
+    menu.classList.toggle('open')
+    hamburgericon.classList.toggle('open')
+}
